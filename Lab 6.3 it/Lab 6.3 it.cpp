8795ea5ea0bb49d77c9a7cd7d30039e0ac985d5d
Volodya <chuchman002@gmail.com>
2023-11-02 18:11:44 +0200

    Add project files.

---
+++ Lab 6.3 it/Lab 6.3 it.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib> 
+
+template <typename T>
+void createArray(std::vector<T>& arr, int size) {
+    arr.clear();
+    for (int i = 0; i < size; i++) {
+        T num = rand() % 100; 
+        arr.push_back(num);
+    }
+}
+
+template <typename T>
+void printArray(const std::vector<T>& arr) {
+    for (const T& num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+template <typename T>
+T Sum(const std::vector<T>& arr) {
+    T sum = 0;
+    for (const T& num : arr) {
+        sum += num;
+    }
+    return sum;
+}
+
+int main() {
+    int size = 10;
+    std::vector<int> arr;
+
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    createArray(arr, size);
+    std::cout << "masive:" << std::endl;
+    printArray(arr);
+
+    int sum = Sum(arr);
+    std::cout << "sum masive elements : " << sum << std::endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <vector>
+#include "../Lab 6.3 it/Lab 6.3 it.cpp" 
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestComputeSum)
+        {
+            std::vector<int> input = { 1, 2, 3, 4, 5 };
+
+            int expected = 15;
+
+            int result = Sum(input);
+
+            Assert::AreEqual(expected, result);
+        }
+    };
+}
